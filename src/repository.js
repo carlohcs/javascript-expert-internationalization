c64083a253f1c8bf1231aaa6de5006d3e0dc19ea
carlohcs <carlohcs@gmail.com>
2022-06-18 14:05:20 -0300

    Add reference to import.meta

--- src/repository.js
+++ src/repository.js
@@ -2,6 +2,7 @@ import { writeFile, readFile } from "fs/promises"
 
 export const save = async (data) => {
   // doesn't exists: __filename, __dirname
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import.meta
   const { pathname: databaseFile } = new URL('database.json', import.meta.url);
   // console.log('databaseFile: ', databaseFile);
   const currentData = JSON.parse(await readFile(databaseFile))
